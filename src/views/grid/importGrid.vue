1946511d323039b0dcc157c4667c79ab14c07a90
yangyifanneu <yangyifanneu@163.com>
2020-03-13 17:42:14 +0800

    完善表格导入

--- src/views/grid/importGrid.vue
+++ src/views/grid/importGrid.vue
@@ -41,43 +41,48 @@ export default {
         var data = new Uint8Array(e.target.result);
         // eslint-disable-next-line no-unused-vars
         var workbook = XLSX.read(data, {type: 'array'});
-        let tabledata = XLSX.utils.sheet_to_json(workbook.Sheets.table);
-        let columns = [];
-        let rowData = [];
-        if (tabledata.length > 0) {
-          let index = 0;
-          for (const key in tabledata[0]) {
-            if (tabledata[0].hasOwnProperty(key)) {
-              columns.push({
-                name: 'column' + index,
-                label: key,
-                key: 'column' + index,
-                title: key,
-                dataIndex: 'column' + index,
-              });
-              index++;
-            }
-          }
-          rowData = tabledata.map((item, index) => {
-            let newObj = {key: index};
-            for (const key in item) {
-              if (item.hasOwnProperty(key)) {
-                for (let i = 0; i < columns.length; i++) {
-                  const headItem = columns[i];
-                  if (key == headItem.label) {
-                    newObj[headItem.name] = item[key];
-                    break;
-                  }
+        for (const sheetName in workbook.Sheets) {
+          if (workbook.Sheets.hasOwnProperty(sheetName)) {
+            const sheetItem = workbook.Sheets[sheetName];
+
+            let tabledata = XLSX.utils.sheet_to_json(sheetItem);
+            let columns = [];
+            let rowData = [];
+            if (tabledata.length > 0) {
+              let index = 0;
+              for (const key in tabledata[0]) {
+                if (tabledata[0].hasOwnProperty(key)) {
+                  columns.push({
+                    name: 'column' + index,
+                    label: key,
+                    key: 'column' + index,
+                    title: key,
+                    dataIndex: 'column' + index,
+                  });
+                  index++;
                 }
               }
+              rowData = tabledata.map((item, index) => {
+                let newObj = {key: index};
+                for (const key in item) {
+                  if (item.hasOwnProperty(key)) {
+                    for (let i = 0; i < columns.length; i++) {
+                      const headItem = columns[i];
+                      if (key == headItem.label) {
+                        newObj[headItem.name] = item[key];
+                        break;
+                      }
+                    }
+                  }
+                }
+                return newObj;
+              });
+              $this.gridStructure = columns;
+              $this.gridData = rowData;
+              break;
             }
-            return newObj;
-          });
+          }
         }
-        $this.gridStructure = columns;
-        $this.gridData = rowData;
-        console.log(columns);
-        console.log(rowData);
       };
       reader.readAsArrayBuffer(f);
       return false;
